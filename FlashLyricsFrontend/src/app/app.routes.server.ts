eacfa1191db7cfb8af070f60c6ef48ca323010d2
robertilintilin <47863210+robertoele@users.noreply.github.com>
2025-09-01 18:32:58 +0100

    Ya se dockeriza toda la aplicación, pero las llamadas me dan un error de CORS

--- FlashLyricsFrontend/src/app/app.routes.server.ts
+++ FlashLyricsFrontend/src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'songs/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'songs/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'search',
     renderMode: RenderMode.Prerender
   }
 ];
