7636b98ba73c942a0eb70cd629e3edd3be134533
robertilintilin <47863210+robertoele@users.noreply.github.com>
2025-09-02 13:52:19 +0100

    FUNCIONA DOCKEEEEEER

--- src/main/java/com/robertoele/FlashLyrics/rest/SongCardRest.java
+++ src/main/java/com/robertoele/FlashLyrics/rest/SongCardRest.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.util.Arrays;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "false")
 public class SongCardRest {
 
     @Autowired
--- src/main/java/com/robertoele/FlashLyrics/rest/SongRest.java
+++ src/main/java/com/robertoele/FlashLyrics/rest/SongRest.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "false")
 public class SongRest {
 
     @Autowired
