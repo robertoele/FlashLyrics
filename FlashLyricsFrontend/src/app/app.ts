e4a7903172645ac211b09e919b6a28216a81dde5
robertilintilin <47863210+robertoele@users.noreply.github.com>
2026-01-10 21:59:20 +0000

    Cabecera con enlace directo a la página principal

--- FlashLyricsFrontend/src/app/app.ts
+++ FlashLyricsFrontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet/>',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
